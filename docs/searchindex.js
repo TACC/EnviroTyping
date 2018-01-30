3244b2f1da323bba39c0e24a2a03b8154f35b85c
Dave Hiltbrand <davehiltbrand@gmail.com>
2018-01-30 11:28:11 -0500

    added docs tree to master

---
+++ docs/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({docnames:["index"],envversion:53,filenames:["index.md"],objects:{},objnames:{},objtypes:{},terms:{index:0,modul:0,page:0,search:0},titles:["Welcome to EnviroTyping\u2019s documentation!"],titleterms:{document:0,envirotyp:0,indic:0,tabl:0,welcom:0}})
