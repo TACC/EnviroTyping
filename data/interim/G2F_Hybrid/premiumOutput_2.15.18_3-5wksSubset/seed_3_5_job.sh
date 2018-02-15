62fe862646f91f71dd81e3a840738d0d7af1191a
Dave Hiltbrand <dhbrand@login3.stampede2.tacc.utexas.edu>
2018-02-15 15:15:49 -0600

    added job using skx-normal queue

---
+++ data/interim/G2F_Hybrid/premiumOutput_2.15.18_3-5wksSubset/seed_3_5_job.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+#
+#SBATCH -J seed_3_5
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -p skx-normal
+#SBATCH -t 48:00:00
+#SBATCH -A Envriotype
+#SBATCH -o slurm.%N.%j.out
+#------------------------------------------------------
+
+remora Rscript --vanilla --verbose ./seed_3_5_TRUE.R > output_seed_3_5/output.Rout 2> output_seed_3_5/error.Rerr
