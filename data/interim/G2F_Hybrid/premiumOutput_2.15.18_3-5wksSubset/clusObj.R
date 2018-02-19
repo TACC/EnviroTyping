baa2eff460fd2d83560bf887c017977f5b6d8d07
Dave Hiltbrand <davehiltbrand@Daves-MacBook-Pro.local>
2018-02-19 10:52:49 -0500

    modified wd for clusObj

--- data/interim/G2F_Hybrid/premiumOutput_2.15.18_3-5wksSubset/clusObj.R
+++ data/interim/G2F_Hybrid/premiumOutput_2.15.18_3-5wksSubset/clusObj.R
@@ -1,6 +1,8 @@
 library(PReMiuM)
-        
-runInfoObj <- readRDS("/output_seed_3_5/runInfoObj.rda")
+
+setwd("/work/04734/dhbrand/stampede2/EnviroTyping/data/interim/G2F_Hybrid/premiumOutput_2.15.18_3-5wksSubset/output_seed_3_5/")
+
+runInfoObj <- readRDS("runInfoObj.rda")
 
 calcDists <- calcDissimilarityMatrix(runInfoObj)
 
