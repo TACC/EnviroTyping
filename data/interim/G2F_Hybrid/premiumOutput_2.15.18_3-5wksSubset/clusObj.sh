37f3756a40278a91a2531b72f94209d68a69baa1
Dave Hiltbrand <dhbrand@login1.stampede2.tacc.utexas.edu>
2018-02-19 09:50:21 -0600

    made job for clusObj

---
+++ data/interim/G2F_Hybrid/premiumOutput_2.15.18_3-5wksSubset/clusObj.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+#
+#SBATCH -J seed_3_5
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -p skx-normal
+#SBATCH -t 03:00:00
+#SBATCH -A Envriotype
+#SBATCH -o slurm.%N.%j.out
+#------------------------------------------------------
+
+Rscript --vanilla --verbose ./clusObj.R > clusObj.Rout
