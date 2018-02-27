0505e5da201114ebcbec75542240d2da65fdabf8
Dave Hiltbrand <davehiltbrand@gmail.com>
2018-02-27 11:43:34 -0500

    prepping hpc jobs files

---
+++ data/interim/G2F_Hybrid/hyb_wks6-8_5000iters/job.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+#
+#SBATCH -J hyb_wks6-8_5000iters
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -p normal
+#SBATCH -t 48:00:00
+#SBATCH -A Envriotype
+#SBATCH -o slurm.%N.%j.out
+#------------------------------------------------------
+mkdir output
+auth-tokens-refresh
+files-get -N data.csv dhbrand/Premium/hybrid_by_weeksSincePlanted_cleaned_weather.csv
+remora Rscript --vanilla --verbose ./job.R > output/output.Rout
