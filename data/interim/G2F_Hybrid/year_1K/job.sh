f57c72e9b45b5c9837edc88880f133f36f8a1cc2
Dave Hiltbrand <davehiltbrand@gmail.com>
2018-02-27 15:32:55 -0500

    staging hpc jobs files

---
+++ data/interim/G2F_Hybrid/year_1K/job.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+#
+#SBATCH -J year_1K
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -p skx-normal
+#SBATCH -t 48:00:00
+#SBATCH -A Envriotype
+#SBATCH -o slurm.%N.%j.out
+#------------------------------------------------------
+mkdir output
+remora Rscript --vanilla --verbose ./job.R > ./output.Rout
