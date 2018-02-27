ce7a2cba34d01eaa3319b2ecfb60ac38cf9a3d79
Dave Hiltbrand <davehiltbrand@gmail.com>
2018-02-27 13:34:47 -0500

    staging hpc jobs files

---
+++ data/interim/G2F_Hybrid/exp_hyb_wks3-5_1K/job.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+#
+#SBATCH -J exp_hyb_wks3-5_1K
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -p skx-normal
+#SBATCH -t 48:00:00
+#SBATCH -A Envriotype
+#SBATCH -o slurm.%N.%j.out
+#------------------------------------------------------
+mkdir output
+auth-tokens-refresh
+files-get -N data.csv dhbrand/Premium/hybrid_by_weeksSincePlanted_cleaned_weather.csv
+remora Rscript --vanilla --verbose ./job.R > ./output.Rout
