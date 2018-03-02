1abf86b57d1bc098ea34d89af6f673131f06981d
Dave Hiltbrand <davehiltbrand@gmail.com>
2018-03-02 15:12:57 -0500

    staging hpc jobs files

---
+++ data/interim/G2F_Hybrid/exp_hyb_wks6-8_by_week_5k/job.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+#
+#SBATCH -J exp_hyb_wks6-8_by_week_5k
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -p skx-normal
+#SBATCH -t 24:00:00
+#SBATCH -A Envriotype
+#SBATCH -o slurm.%N.%j.out
+#------------------------------------------------------
+mkdir output
+remora Rscript --vanilla --verbose ./job.R > ./output.Rout
---
+++ data/interim/G2F_Hybrid/numObs.R
@@ -0,0 +1,28 @@
+library(readr)
+library(dplyr)
+library(lubridate)
+df <- read_csv("hybrid_by_day_cleaned_weather.csv")
+df1 <- read_csv("hybrid_by_week_cleaned_weather.csv")
+
+subset <- df %>% filter(Date >= Planted + weeks(4) & Date <= Planted + weeks(5))
+ss <- df %>% filter(Date >= Planted + weeks(7) & Date <= Planted + weeks(8))
+ss <- df %>% filter(Date >= Planted + weeks(7) & Date <= Planted + weeks(8))
+
+ss4 <- df %>% mutate(Mon = month(Date)) %>% filter(Mon == 4)
+ss5 <- df %>% mutate(Mon = month(Date)) %>% filter(Mon == 5)
+ss6 <- df %>% mutate(Mon = month(Date)) %>% filter(Mon == 6) 
+ss7 <- df %>% mutate(Mon = month(Date)) %>% filter(Mon == 7)
+ss8 <- df %>% mutate(Mon = month(Date)) %>% filter(Mon == 8)
+ss9 <- df %>% mutate(Mon = month(Date)) %>% filter(Mon == 9)
+ss10 <- df %>% mutate(Mon = month(Date)) %>% filter(Mon == 10)
+
+sss4 <- df1 %>% filter(Month == 4)
+sss5 <- df1 %>% filter(Month == 5)
+sss6 <- df1 %>% filter(Month == 6) 
+sss7 <- df1 %>% filter(Month == 7)
+sss8 <- df1 %>% filter(Month == 8)
+sss9 <- df1 %>% filter(Month == 9)
+sss10 <- df1 %>% filter(Month == 10)
+unique(df1$Harvest)
+
+season <- df %>% filter(Date >= Planted & Date <=Harvest)
