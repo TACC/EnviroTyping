6bf59ed0ed08b58b19165ab67c7ffef250a097d2
Dave Hiltbrand <hiltbrandd@uncw.edu>
2018-03-12 12:04:09 -0500

    changed path

--- sandbox/G2F/working_with_plots/optClusters.R
+++ sandbox/G2F/working_with_plots/optClusters.R
@@ -1,6 +1,6 @@
 library(tidyverse)
 library(ClusterR)
-df <- read_csv("../hybrid_by_week_cleaned_weather.csv",col_types = cols("Repl" = col_integer(), "rainMin" = col_number(), "rainMax" = col_number(), "rainMean" = col_number(), "rainMedian" = col_number(), "solarMin" = col_number(), "solarMax" = col_number(), "solarMedian" = col_number(), "windDirMin" = col_number(), "windDirMax" = col_number(), "windDirMedian" = col_number()))
+df <- read_csv("../../../data/interim/G2F_Hybrid/hybrid_by_week_cleaned_weather.csv",col_types = cols("Repl" = col_integer(), "rainMin" = col_number(), "rainMax" = col_number(), "rainMean" = col_number(), "rainMedian" = col_number(), "solarMin" = col_number(), "solarMax" = col_number(), "solarMedian" = col_number(), "windDirMin" = col_number(), "windDirMax" = col_number(), "windDirMedian" = col_number()))
 
 val <- grep("Min|Max",names(df))
 numericVars <- names(df[val])[vapply(df[val], function(x) var(x) != 0, logical(1))]
