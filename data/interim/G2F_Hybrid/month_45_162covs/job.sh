8d0ed9d031c33de46e7de6e111e466fe99f48067
Dave Hiltbrand <davehiltbrand@gmail.com>
2018-04-18 17:19:46 -0400

    staging hpc jobs

--- data/interim/G2F_Hybrid/month_45_162covs/job.sh
+++ data/interim/G2F_Hybrid/month_45_162covs/job.sh
@@ -3,13 +3,12 @@
 #
 #SBATCH -J cov162
 #SBATCH -N 1
-#SBATCH -n 2
+#SBATCH -n 1
 #SBATCH -p skx-normal
 #SBATCH -t 48:00:00
 #SBATCH -A Envriotype
 #SBATCH -o job_%j_%N.out
 #------------------------------------------------------
-
-Rscript --vanilla --verbose ./iters200/month_45_162covs.R > ./iters200/output.Rout &
-Rscript --vanilla --verbose ./iters400/month_45_162covs.R > ./iters400/output.Rout 
+mkdir output
+remora Rscript --vanilla --verbose ./month_45_162covs.R > ./output.Rout
 
