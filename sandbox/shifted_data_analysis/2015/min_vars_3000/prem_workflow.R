bec427b651131be0e2aa863d36d2f1ff45a2c148
Dave Hiltbrand <davehiltbrand@gmail.com>
2018-05-23 11:36:17 -0400

    staging jobs for hpc

---
+++ sandbox/shifted_data_analysis/2015/min_vars_3000/prem_workflow.R
@@ -0,0 +1,17 @@
+library(PReMiuM)
+library(tidyverse)
+
+setwd("/work/04734/dhbrand/stampede2/github/EnviroTyping/sandbox/shifted_data_analysis/2015/min_vars_3000/output")
+
+df <- read_rds("../../../../../data/interim/2015/hybrid_by_month_shift_all_stats.rds")
+
+variance.var <- names(which(map_dbl(df[,17:189], var, na.rm = TRUE) != 0))
+min.vars <- str_subset(variance.var, "Min")
+
+set.seed(1234)
+runInfoObj <- profRegr(covNames, outcome = 'Yield', yModel = 'Normal', xModel = "Mixed", discreteCovs = "Pedi", continuousCovs = min.vars, data = df, nSweeps = 3000, nBurn = 50, nProgress = 100, nClusInit = 1000)
+calcDists <- calcDissimilarityMatrix(runInfoObj)
+clusObj <- calcOptimalClustering(calcDists)
+riskProfObj <- calcAvgRiskAndProfile(clusObj)
+write(riskProfObj, "../riskProfObj.rds")
+
