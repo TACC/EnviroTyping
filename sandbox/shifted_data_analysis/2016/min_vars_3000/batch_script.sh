6f3adc1d3dcec8675a1f8ff0f5475bd57b415e13
Dave Hiltbrand <davehiltbrand@gmail.com>
2018-05-23 10:20:57 -0400

    staging jobs for hpc

---
+++ sandbox/shifted_data_analysis/2016/min_vars_3000/batch_script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+#
+#SBATCH -J min_3000
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -p skx-normal
+#SBATCH -t 24:00:00
+#SBATCH -A Envriotype
+#SBATCH -o job_%j_%N.out
+#------------------------------------------------------
+mkdir output
+Rscript --vanilla --verbose ./prem_workflow.R > ./output.Rout
