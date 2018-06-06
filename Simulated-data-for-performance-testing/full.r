611701ae915a43be7da0039a760e6fc121538515
Dave Hiltbrand <hiltbrandd@uncw.edu>
2018-06-06 07:27:55 -0500

    full simulated test

--- Simulated-data-for-performance-testing/full.r
+++ Simulated-data-for-performance-testing/full.r
@@ -1,3 +1,5 @@
+
+
 # test of the simulated data 
 
 dataAll<-read.table("sim_large_dataset_NormalMixed.dat",header = TRUE)
@@ -5,13 +7,13 @@ dataAll<-read.table("sim_large_dataset_NormalMixed.dat",header = TRUE)
 # this is a small test using only 2 continuous covariates and the first 2,000 observations
 
 library(PReMiuM)
+library(readr)
 runInfoObj<-profRegr(yModel="Normal", 
                      xModel="Normal", nSweeps=2000, nClusInit=40,
-                     nBurn=2000, data=dataAll[1:2000,], output="output", 
+                     nBurn=2000, data=dataAll, output="output", 
                      covNames = c("Cont1","Cont2"))
 
 dissimObj<-calcDissimilarityMatrix(runInfoObj)
 clusObj<-calcOptimalClustering(dissimObj)
 riskProfileObj<-calcAvgRiskAndProfile(clusObj)
-clusterOrderObj<-plotRiskProfile(riskProfileObj,"summary.png")
-
+write_rds(riskProfileObj, "riskProfObj.rds", compress = "xz")
