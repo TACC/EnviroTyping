bbcbe5776808a54726f8197a222dd85a164f36bf
Dave Hiltbrand <davehiltbrand@gmail.com>
2018-06-14 15:16:20 -0400

    updated error in stations

--- sandbox/interim_datasets/2015/by_month_shift_front.R
+++ sandbox/interim_datasets/2015/by_month_shift_front.R
@@ -2,34 +2,32 @@ library(tidyverse)
 library(lubridate)
 library(data.table)
 
-df <- read_rds("~/GitHub/EnviroTyping/data/interim/G2F_Hybrid/hybrid_by_month_calibrated_weather.rds")
-table(df$Month,df$StatID)
+df <- read_rds("~/github/EnviroTyping/data/interim/2015/hyb_by_mon_calib.rds")
+table(df$month,df$stat_id)
 
 
-s3 <- df %>% filter(StatID == 8428) %>% mutate(Month = replace(Month, between(Month,3,7), c(5:9)))
-s4 <- df %>% filter(StatID == 8427) %>% mutate(Month = replace(Month, between(Month,4,8), c(5:9)))
-s5 <- df %>% filter(StatID == 8658) %>% mutate(Month = replace(Month, between(Month,4,7), c(5:8)))
-df <- df %>% filter(!(StatID %in% c(8428,8427,8658,8651))) 
+s3 <- df %>% filter(stat_id == 8428) %>% mutate(month = replace(month, between(month,3,7), c(5:9)))
+s4 <- df %>% filter(stat_id == 8427) %>% mutate(month = replace(month, between(month,4,8), c(5:9)))
+s5 <- df %>% filter(stat_id == 8658) %>% mutate(month = replace(month, between(month,4,7), c(5:8)))
+df <- df %>% filter(!(stat_id %in% c(8428,8427,8658,8651))) 
 
 df <- df %>% bind_rows(.,s3,s4)
 
 
-#df <- df %>% mutate(Month = replace(Month, between(Month,5,9),c(1:5)))
-df <- df %>% filter(Month %in% c(5:9))
+#df <- df %>% mutate(month = replace(month, between(month,5,9),c(1:5)))
+df <- df %>% filter(month %in% c(5:10))
 
-table(df$Month,df$StatID)
+table(df$month,df$stat_id)
 
-val <- grep("Median|Min|Max|Mean",names(df))
-numericVars <- names(which(map_dbl(df[val], var) != 0))
+df1 <- df %>% select(stat_id, pedi, repl, yield, month, numeric_vars)
+table(df1$Pedi, df1$stat_id)
 
-df1 <- df %>% select(StatID, Pedi, Repl, Yield, Month, numericVars)
-table(df1$Pedi, df1$StatID)
-
-df2 <- df1 %>% 
-    gather(Var,val,6:42) %>% 
-    unite(Var1, Var, Month) %>% 
+df2 <- df %>% 
+    gather(Var,val,17:56) %>% 
+    unite(Var1, Var, month) %>% 
     spread(Var1, val)
 
+check <- df2 %>% select(stat_id, contains("mean")) %>% filter(stat_id %in% c(8427:8428))
 
 
-write_rds(df2, "~/GitHub/EnviroTyping/data/interim/G2F_Hybrid/hybrid_by_month_shift_all_stats.rds", compress = "xz")
+write_rds(df2, "~/GitHub/EnviroTyping/data/interim/2015/hyb_by_mon_calib_wide_shifted", compress = "xz")
