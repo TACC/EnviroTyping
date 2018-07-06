721279db91e6a1eac5628190d53fbc4b33530ace
Austin Gratton <grattonauz@gmail.com>
2018-07-06 15:58:24 -0400

    capitalization issue

--- sandbox/shifted_data_analysis/2015/min_vars_3000/prem_workflow.R
+++ sandbox/shifted_data_analysis/2015/min_vars_3000/prem_workflow.R
@@ -6,7 +6,7 @@ setwd("/work/04902/azg5169/stampede2/EnviroTyping/sandbox/shifted_data_analysis/
 df <- read_rds("../../../../../data/interim/2015/hyb_by_mon_calib_wide_shifted.rds")
 
 variance.var <- names(which(map_dbl(df[,16:255], var, na.rm = TRUE) != 0))
-min.vars <- str_subset(variance.var, "Min")
+min.vars <- str_subset(variance.var, "min")
 
 set.seed(1234)
 runInfoObj <- profRegr(covNames, outcome = 'Yield', yModel = 'Normal', xModel = "Mixed", discreteCovs = "Pedi", continuousCovs = min.vars, data = df, nSweeps = 3000, nBurn = 50, nProgress = 100, nClusInit = 1000, seed = 2435)
