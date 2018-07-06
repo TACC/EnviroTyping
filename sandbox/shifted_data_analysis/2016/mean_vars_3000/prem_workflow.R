12d025462cbea0215c575c12f5f9ec0242014d59
Austin Gratton <grattonauz@gmail.com>
2018-07-06 15:45:00 -0400

    fix capitalization

--- sandbox/shifted_data_analysis/2016/mean_vars_3000/prem_workflow.R
+++ sandbox/shifted_data_analysis/2016/mean_vars_3000/prem_workflow.R
@@ -1,7 +1,7 @@
 library(PReMiuM)
 library(tidyverse)
 
-setwd("/work/04902/azg5169/stampede2/EnviroTyping/sandbox/shifted_data_analysis/2014/min_vars_3000/output")
+setwd("/work/04902/azg5169/stampede2/EnviroTyping/sandbox/shifted_data_analysis/2016/min_vars_3000/output")
 
 df <- read_rds("../../../../../data/interim/2016/hyb_by_mon_calib_wide_shifted.rds")
 
@@ -9,7 +9,7 @@ variance.var <- names(which(map_dbl(df[,16:255], var, na.rm = TRUE) != 0))
 mean.vars <- str_subset(variance.var, "mean")
 
 set.seed(1234)
-runInfoObj <- profRegr(covNames, outcome = 'yield', yModel = 'Normal', xModel = "Mixed", discreteCovs = "pedi", continuousCovs = mean.vars, data = df, nSweeps = 3000, nBurn = 50, nProgress = 100, nClusInit = 1000, seed = 2435)
+runInfoObj <- profRegr(covNames, outcome = 'Yield', yModel = 'Normal', xModel = "Mixed", discreteCovs = "Pedi", continuousCovs = mean.vars, data = df, nSweeps = 3000, nBurn = 50, nProgress = 100, nClusInit = 1000, seed = 2435)
 calcDists <- calcDissimilarityMatrix(runInfoObj)
 clusObj <- calcOptimalClustering(calcDists)
 riskProfObj <- calcAvgRiskAndProfile(clusObj)
