485cd38827947e2628a137f41787db377386704b
Austin Gratton <grattonauz@gmail.com>
2018-07-06 16:02:30 -0400

    capitalization fix

--- sandbox/shifted_data_analysis/2015/min_vars_3000/prem_workflow.R
+++ sandbox/shifted_data_analysis/2015/min_vars_3000/prem_workflow.R
@@ -8,8 +8,9 @@ df <- read_rds("../../../../../data/interim/2015/hyb_by_mon_calib_wide_shifted.r
 variance.var <- names(which(map_dbl(df[,16:255], var, na.rm = TRUE) != 0))
 min.vars <- str_subset(variance.var, "min")
 
+pwd
 set.seed(1234)
-runInfoObj <- profRegr(covNames, outcome = 'Yield', yModel = 'Normal', xModel = "Mixed", discreteCovs = "Pedi", continuousCovs = min.vars, data = df, nSweeps = 3000, nBurn = 50, nProgress = 100, nClusInit = 1000, seed = 2435)
+runInfoObj <- profRegr(covNames, outcome = 'yield', yModel = 'Normal', xModel = "Mixed", discreteCovs = "pedi", continuousCovs = min.vars, data = df, nSweeps = 3000, nBurn = 50, nProgress = 100, nClusInit = 1000, seed = 2435)
 calcDists <- calcDissimilarityMatrix(runInfoObj)
 clusObj <- calcOptimalClustering(calcDists)
 riskProfObj <- calcAvgRiskAndProfile(clusObj)
