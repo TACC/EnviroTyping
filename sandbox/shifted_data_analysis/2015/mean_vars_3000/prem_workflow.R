7ecc092e1f9248ea50dd0db64cd90c9eccf9bcd2
Austin Gratton <grattonauz@gmail.com>
2018-07-06 15:49:27 -0400

    fix capitalization

--- sandbox/shifted_data_analysis/2015/mean_vars_3000/prem_workflow.R
+++ sandbox/shifted_data_analysis/2015/mean_vars_3000/prem_workflow.R
@@ -6,10 +6,10 @@ setwd("/work/04902/azg5169/stampede2/EnviroTyping/sandbox/shifted_data_analysis/
 df <- read_rds("../../../../../data/interim/2015/hyb_by_mon_calib_wide_shifted.rds")
 
 variance.var <- names(which(map_dbl(df[,16:255], var, na.rm = TRUE) != 0))
-mean.vars <- str_subset(variance.var, "Mean")
+mean.vars <- str_subset(variance.var, "mean")
 
 set.seed(1234)
-runInfoObj <- profRegr(covNames, outcome = 'yield', yModel = 'Normal', xModel = "Mixed", discreteCovs = "Pedi", continuousCovs = mean.vars, data = df, nSweeps = 3000, nBurn = 50, nProgress = 100, nClusInit = 1000, seed = 2435)
+runInfoObj <- profRegr(covNames, outcome = 'yield', yModel = 'Normal', xModel = "Mixed", discreteCovs = "pedi", continuousCovs = mean.vars, data = df, nSweeps = 3000, nBurn = 50, nProgress = 100, nClusInit = 1000, seed = 2435)
 calcDists <- calcDissimilarityMatrix(runInfoObj)
 clusObj <- calcOptimalClustering(calcDists)
 riskProfObj <- calcAvgRiskAndProfile(clusObj)
