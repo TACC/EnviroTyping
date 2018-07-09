05e6e713eb24bf2644ea390f495560cf67f33a65
Austin Gratton <azg5169@login4.stampede2.tacc.utexas.edu>
2018-07-09 12:51:30 -0500

    1000 1000 sweeps for mean

---
+++ sandbox/shifted_data_analysis/2015/mean_vars_1000/prem_workflow.R
@@ -0,0 +1,17 @@
+library(PReMiuM)
+library(tidyverse)
+
+setwd("/work/04902/azg5169/stampede2/EnviroTyping/sandbox/shifted_data_analysis/2015/min_vars_1000/output")
+
+df <- read_rds("../../../../../data/interim/2015/hyb_by_mon_calib_wide_shifted.rds")
+
+variance.var <- names(which(map_dbl(df[,16:255], var, na.rm = TRUE) != 0))
+mean.vars <- str_subset(variance.var, "mean")
+
+set.seed(1234)
+runInfoObj <- profRegr(covNames, outcome = 'yield', yModel = 'Normal', xModel = "Mixed", discreteCovs = "pedi", continuousCovs = mean.vars, data = df, nSweeps = 3000, nBurn = 50, nProgress = 100, nClusInit = 1000, seed = 2435)
+calcDists <- calcDissimilarityMatrix(runInfoObj)
+clusObj <- calcOptimalClustering(calcDists)
+riskProfObj <- calcAvgRiskAndProfile(clusObj)
+write_rds(riskProfObj, "../riskProfObj.rds")
+
