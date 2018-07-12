a4adcc508f597d24a4b351fcbed205b1ab4365bf
Austin Gratton <grattonauz@gmail.com>
2018-07-12 13:44:53 -0400

    added email notifications

--- sandbox/shifted_data_analysis/2015/mean_vars_3000/batch_script.sh
+++ sandbox/shifted_data_analysis/2015/mean_vars_3000/batch_script.sh
@@ -4,8 +4,10 @@
 #SBATCH -J mean_3K_15
 #SBATCH -N 1
 #SBATCH -n 1
-#SBATCH -p skx-normal
-#SBATCH -t 48:00:00
+#SBATCH --mail-user=azg5169@uncw.edu
+#SBATCH --mail-type=all    # Send email at begin and end of job
+#SBATCH -p long
+#SBATCH -t 96:00:00
 #SBATCH -A Envriotype
 #SBATCH -o job_%j_%N.out
 #------------------------------------------------------
