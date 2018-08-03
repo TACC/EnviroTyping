598ebc2c4c6d39a2cd0c37a90f73b0be76fc3cbf
Dave Hiltbrand <davehiltbrand@gmail.com>
2018-08-03 13:52:25 -0400

    added measure for empty clusters

--- sandbox/testing_seeds/small_loop_on_st2.R
+++ sandbox/testing_seeds/small_loop_on_st2.R
@@ -10,18 +10,24 @@ variance_var <- names(which(map_dbl(df[,16:length(df)], var, na.rm = TRUE) != 0)
 min_vars <- str_subset(variance_var, "min")
 
 set.seed(1234)
-seed <- sample(1:10000,10)
+seed <- sample(1:100000,10)
 tic.clearlog()
 for (i in seed) {
     tic()
     dir_name <- paste0("/work/04734/dhbrand/stampede2/github/EnviroTyping/sandbox/testing_seeds/", i, "_output")
     dir.create(dir_name)
     setwd(dir_name)
-    runInfoObj <- profRegr(covNames, outcome = 'yield', yModel = 'Normal', xModel = "Mixed", discreteCovs = "pedi", continuousCovs = min_vars, data = df, nSweeps = 20, nBurn = 1, nProgress = 1, seed = i)
+    runInfoObj <- profRegr(covNames, outcome = 'yield', yModel = 'Normal', xModel = "Mixed", discreteCovs = "pedi", continuousCovs = min_vars, data = df, nSweeps = 500, nBurn = 10, nProgress = 25, seed = i)
     toc(log = TRUE, quiet = TRUE)
     clus_file <- paste0(dir_name,"/output_nMembers.txt")
-    readLines(clus_file)
-    }
+    nmembers <- str_split(read_lines(clus_file), pattern = " ", simplify = TRUE)
+    empty_clusters <- nmembers %>% 
+        as.tibble() %>% 
+        mutate_all(funs(.==0)) %>% 
+        reduce(`+`) %>% 
+        as.tibble()
+    write_delim(empty_clusters, "empty_clusters.txt")
+}
 
 seed_v_time <- bind_cols(seed = seed, time = unlist(tic.log(format = TRUE)))
 
