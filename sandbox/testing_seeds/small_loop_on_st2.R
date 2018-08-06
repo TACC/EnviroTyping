359c7a7120e6ad7f637d2cd978970417b57030d8
Austin Gratton <grattonauz@gmail.com>
2018-08-06 14:02:44 -0400

    comment out cluster stuff

--- sandbox/testing_seeds/small_loop_on_st2.R
+++ sandbox/testing_seeds/small_loop_on_st2.R
@@ -2,9 +2,9 @@ library(PReMiuM)
 library(tidyverse)
 library(tictoc)
 
-setwd("/work/04902/azg5169/stampede2/EnviroTyping/sandbox/testing_seeds")
+setwd("~/GitHub/EnviroTyping/sandbox/testing_seeds")
 
-df <- read_rds("/work/04902/azg5169/stampede2/EnviroTyping/data/interim/2015/hyb_by_mon_calib_wide_w_wth_nas.rds")
+df <- read_rds("~/GitHub/EnviroTyping/data/interim/2015/hyb_by_mon_calib_wide_w_wth_nas.rds")
 
 variance_var <- names(which(map_dbl(df[,16:length(df)], var, na.rm = TRUE) != 0))
 min_vars <- str_subset(variance_var, "min")
@@ -14,19 +14,19 @@ seed <- sample(1:100000,10)
 tic.clearlog()
 for (i in seed) {
     tic()
-    dir_name <- paste0("/work/04902/azg5169/stampede2/EnviroTyping/sandbox/testing_seeds", i, "_output")
+    dir_name <- paste0("~/GitHub/EnviroTyping/sandbox/testing_seeds", i, "_output")
     dir.create(dir_name)
     setwd(dir_name)
-    runInfoObj <- profRegr(covNames, outcome = 'yield', yModel = 'Normal', xModel = "Mixed", discreteCovs = "pedi", continuousCovs = min_vars, data = df, nSweeps = 500, nBurn = 10, nProgress = 25, seed = i)
+    runInfoObj <- profRegr(covNames, outcome = 'yield', yModel = 'Normal', xModel = "Mixed", discreteCovs = "pedi", continuousCovs = min_vars, data = df, nSweeps = 20, nBurn = 10, nProgress = 1, seed = i)
     toc(log = TRUE, quiet = TRUE)
-    clus_file <- paste0(dir_name,"/output_nMembers.txt")
-    nmembers <- str_split(read_lines(clus_file), pattern = " ", simplify = TRUE)
-    empty_clusters <- nmembers %>% 
-        as.tibble() %>% 
-        mutate_all(funs(.==0)) %>% 
-        reduce(`+`) %>% 
-        as.tibble()
-    write_delim(empty_clusters, "empty_clusters.txt")
+    #clus_file <- paste0(dir_name,"/output_nMembers.txt")
+    #nmembers <- str_split(read_lines(clus_file), pattern = " ", simplify = TRUE)
+    #empty_clusters <- nmembers %>% 
+        #as.tibble() %>% 
+        #mutate_all(funs(.==0)) %>% 
+        #reduce(`+`) %>% 
+        #as.tibble()
+    #write_delim(empty_clusters, "empty_clusters.txt")
 }
 
 seed_v_time <- bind_cols(seed = seed, time = unlist(tic.log(format = TRUE)))
