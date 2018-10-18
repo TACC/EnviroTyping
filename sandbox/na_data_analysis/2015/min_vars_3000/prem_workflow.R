9d2ca542122213d68895e5f3a0aa1dabb621ef54
Austin Gratton <grattonauz@gmail.com>
2018-10-18 10:24:47 -0400

    clean na code

--- sandbox/na_data_analysis/2015/min_vars_3000/prem_workflow.R
+++ sandbox/na_data_analysis/2015/min_vars_3000/prem_workflow.R
@@ -4,14 +4,14 @@ library(tictoc)
 
 setwd("/work/04902/azg5169/stampede2/EnviroTyping/sandbox/shifted_data_analysis/2015/min_vars_3000/output")
 
-df <- read_rds("~/github/EnviroTyping/data/interim/2015/hyb_by_mon_calib_w_wth_nas.rds")
+df <- read_rds("../../../../../data/interim/2015/hyb_by_mon_calib_w_wth_nas.rds")
 
 variance_var <- names(which(map_dbl(df[,16:length(df)], var, na.rm = TRUE) != 0))
 min_vars <- str_subset(variance_var, "min")
 
-set.seed(1234)
+set.seed(12345)
 tic()
-runInfoObj <- profRegr(covNames, outcome = 'yield', yModel = 'Normal', xModel = "Mixed", discreteCovs = "pedi", continuousCovs = min_vars, data = df, nSweeps = 3000, nBurn = 50, nProgress = 100, nClusInit = 1000, seed = 2435)
+runInfoObj <- profRegr(covNames, outcome = 'yield', yModel = 'Normal', xModel = "Mixed", discreteCovs = "pedi", continuousCovs = min_vars, data = df, nSweeps = 3000, nBurn = 50, nProgress = 100, nClusInit = 1000, seed = 12345)
 toc()
 calcDists <- calcDissimilarityMatrix(runInfoObj)
 clusObj <- calcOptimalClustering(calcDists)
--- sandbox/na_data_analysis/2016/min_vars_3000/prem_workflow.R
+++ sandbox/na_data_analysis/2016/min_vars_3000/prem_workflow.R
@@ -10,8 +10,8 @@ df <- read_rds("../../../../../data/interim/2016/hyb_by_mon_calib_w_wth_nas.rds"
 variance.var <- names(which(map_dbl(df[,16:255], var, na.rm = TRUE) != 0))
 min.vars <- str_subset(variance.var, "min")
 
-set.seed(1234)
-runInfoObj <- profRegr(covNames, outcome = 'Yield', yModel = 'Normal', xModel = "Mixed", discreteCovs = "Pedi", continuousCovs = min.vars, data = df, nSweeps = 3000, nBurn = 50, nProgress = 100, nClusInit = 1000, seed = 2435)
+set.seed(12345)
+runInfoObj <- profRegr(covNames, outcome = 'yield', yModel = 'Normal', xModel = "Mixed", discreteCovs = "pedi", continuousCovs = min.vars, data = df, nSweeps = 3000, nBurn = 50, nProgress = 100, nClusInit = 1000, seed = 12345)
 calcDists <- calcDissimilarityMatrix(runInfoObj)
 clusObj <- calcOptimalClustering(calcDists)
 riskProfObj <- calcAvgRiskAndProfile(clusObj)
