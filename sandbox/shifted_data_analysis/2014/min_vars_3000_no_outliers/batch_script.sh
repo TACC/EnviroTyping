97d03cdeb20c13db8e3c69a7ea6a4ea98b065846
Austin Gratton <grattonauz@gmail.com>
2018-10-23 13:04:13 -0400

    test without outlier

--- sandbox/shifted_data_analysis/2014/min_vars_3000_no_outliers/batch_script.sh
+++ sandbox/shifted_data_analysis/2014/min_vars_3000_no_outliers/batch_script.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #
 #
-#SBATCH -J min3K14no
+#SBATCH -J 3k14no
 #SBATCH -N 1
 #SBATCH -n 1
 #SBATCH -p skx-normal
-#SBATCH -t 48:00:00
+#SBATCH -t 24:00:00
 #SBATCH -A Envriotype
 #SBATCH -o job_%j_%N.out
 #------------------------------------------------------
