74efe8f9c6450d5d7f186ac89e3e31235d280e44
Banks Osborne <banks@Bankss-MacBook-Pro.local>
2018-11-13 13:46:13 -0500

    New folder

---
+++ sandbox/working_with_plots/BanksPlots/batch_script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+#
+#SBATCH -J min163k
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -p skx-normal
+#SBATCH -t 10:00:00
+#SBATCH -A Envriotype
+#SBATCH -o job_%j_%N.out
+#------------------------------------------------------
+mkdir -p output
+Rscript --verbose ./prem_workflow.R > ./output.Rout
