3cc7b23715c392d0b94ac3c47441108caf2fd77b
Banks Osborne <banks@Bankss-MacBook-Pro.local>
2018-12-18 15:08:48 -0500

    Update batch script

--- sandbox/working_with_plots/BanksPlots/batch_script.sh
+++ sandbox/working_with_plots/BanksPlots/batch_script.sh
@@ -10,4 +10,4 @@
 #SBATCH -o job_%j_%N.out
 #------------------------------------------------------
 mkdir -p output
-Rscript --verbose ./2016JuneYieldCalibWide.R > ./output.Rout
+Rscript --verbose ./2016YieldCalibWide.R > ./output.Rout
