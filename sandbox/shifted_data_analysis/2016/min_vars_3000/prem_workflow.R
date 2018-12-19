3acb24178165907c11fa7503f931f4b61bbbaa2e
AustinGratton <grattonauz@gmail.com>
2018-12-19 14:36:55 -0500

    clear environment

--- sandbox/shifted_data_analysis/2016/min_vars_3000/prem_workflow.R
+++ sandbox/shifted_data_analysis/2016/min_vars_3000/prem_workflow.R
@@ -1,6 +1,8 @@
 library(PReMiuM)
 library(tidyverse)
 
+rm(list = ls(all = TRUE))
+
 setwd("/work/04902/azg5169/stampede2/EnviroTyping/sandbox/shifted_data_analysis/2016/min_vars_3000/output")
 
 df <- read_rds("../../../../../data/interim/2016/hyb_by_mon_calib_wide_shifted.rds")
