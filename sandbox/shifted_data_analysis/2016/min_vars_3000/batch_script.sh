7b12a0f8b653b31836bd925a781d6c71aae1ff68
AustinGratton <grattonauz@gmail.com>
2018-12-19 11:50:47 -0500

    reverting single run to get results

--- sandbox/shifted_data_analysis/2016/min_vars_3000/batch_script.sh
+++ sandbox/shifted_data_analysis/2016/min_vars_3000/batch_script.sh
@@ -7,7 +7,9 @@
 #SBATCH -p skx-normal
 #SBATCH -t 15:00:00
 #SBATCH -A Envriotype
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=azg5169@azg5169.com
 #SBATCH -o job_%j_%N.out
 #------------------------------------------------------
-mkdir -p output1
+mkdir -p output
 Rscript --verbose ./prem_workflow.R > ./output.Rout
--- sandbox/shifted_data_analysis/2016/min_vars_3000/prem_workflow.R
+++ sandbox/shifted_data_analysis/2016/min_vars_3000/prem_workflow.R
@@ -1,7 +1,7 @@
 library(PReMiuM)
 library(tidyverse)
 
-setwd("/work/04902/azg5169/stampede2/EnviroTyping/sandbox/shifted_data_analysis/2016/min_vars_3000/output1")
+setwd("/work/04902/azg5169/stampede2/EnviroTyping/sandbox/shifted_data_analysis/2016/min_vars_3000/output")
 
 df <- read_rds("../../../../../data/interim/2016/hyb_by_mon_calib_wide_shifted.rds")
 
@@ -11,10 +11,10 @@ min.vars <- str_subset(variance.var, "min")
 set.seed(12345)
 
 
-runInfoObj <- profRegr(covNames, outcome = 'Yield', yModel = 'Normal', xModel = "Mixed", discreteCovs = "Pedi", continuousCovs = min.vars, data = df, nSweeps = 3000, nBurn = 50, nProgress = 1000, nClusInit = 1000)
+runInfoObj <- profRegr(covNames, outcome = 'Yield', yModel = 'Normal', xModel = "Mixed", discreteCovs = "Pedi", continuousCovs = min.vars, data = df, nSweeps = 3000, nBurn = 1000, nProgress = 1000, nClusInit = 1000)
 calcDists <- calcDissimilarityMatrix(runInfoObj)
-clusObj <- calcOptimalClustering(calcDists)
-riskProfObj <- calcAvgRiskAndProfile(clusObj)
-write_rds(clusObj, "../clusObj1.rds", compress = "xz")
+clusObj1 <- calcOptimalClustering(calcDists)
+riskProfObj <- calcAvgRiskAndProfile(clusObj1)
+write_rds(clusObj1, "../clusObj1.rds", compress = "xz")
 
 
