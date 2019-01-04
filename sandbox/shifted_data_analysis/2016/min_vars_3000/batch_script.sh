7c72b44207d3f34383de1bb0718a1183a0d3b28d
Banks Osborne <banks@Bankss-MacBook-Pro.local>
2019-01-04 00:30:11 -0500

    Change name

--- sandbox/shifted_data_analysis/2016/min_vars_3000/batch_script.sh
+++ sandbox/shifted_data_analysis/2016/min_vars_3000/batch_script.sh
@@ -5,9 +5,9 @@
 #SBATCH -N 1
 #SBATCH -n 1
 #SBATCH -p skx-normal
-#SBATCH -t 10:00:00
+#SBATCH -t 15:00:00
 #SBATCH -A Envriotype
 #SBATCH -o job_%j_%N.out
 #------------------------------------------------------
 mkdir -p output
-Rscript --verbose ./prem_workflow.R > ./output.Rout
+Rscript --verbose ./prem_workflow3.R > ./output.Rout
