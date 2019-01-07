d394f959b55e87a4eea40b484ce6355ea287c22e
Banks Osborne <banks@Bankss-MacBook-Pro.local>
2019-01-07 17:27:53 -0500

    Change directory

--- sandbox/shifted_data_analysis/2016/min_vars_3000/riskOutput.R
+++ sandbox/shifted_data_analysis/2016/min_vars_3000/riskOutput.R
@@ -1,12 +1,12 @@
 library(PReMiuM)
 library(tidyverse)
 
-# Use 2016 wide, shifted, all months, min_vars_3000 clusObj to create riskProfObj
+# Use 2016 wide, shifted, all months, w/o outliers, min_vars_3000 clusObj to create riskProfObj
 
-setwd("/work/06019/bno5761/stampede2/EnviroTyping/sandbox/shifted_data_analysis/2016/min_vars_3000")
+setwd("/work/06019/bno5761/stampede2/EnviroTyping/sandbox/shifted_data_analysis/2016/min_vars_3000_no_outliers")
 
 clusObj = read_rds("clusObj.rds")
 
 riskProfObj = calcAvgRiskAndProfile(clusObj)
 
-write_rds(riskProfObj, "/riskProfObj.rds", compress = "xz")
+write_rds(riskProfObj, "/riskProfObj.rds", compress = "xz") 
