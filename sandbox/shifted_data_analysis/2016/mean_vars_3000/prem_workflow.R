fceae10d46e9fc0ba9e16da731d0d2b37dd84184
Banks Osborne <banks@Bankss-MacBook-Pro.local>
2019-01-13 22:40:13 -0500

    Adapt

--- sandbox/shifted_data_analysis/2016/mean_vars_3000/prem_workflow.R
+++ sandbox/shifted_data_analysis/2016/mean_vars_3000/prem_workflow.R
@@ -1,17 +1,21 @@
-library(PReMiuM)
+### AUTHOR: Banks Osborne
+### DATE: 13 January 2019
+
 library(tidyverse)
+library(PReMiuM)
 
-setwd("/work/04902/azg5169/stampede2/EnviroTyping/sandbox/shifted_data_analysis/2016/min_vars_3000/output")
+setwd("/Users/banks/RProjects/EnviroTyping/sandbox/shifted_data_analysis/2016/mean_vars_3000/output")
 
-df <- read_rds("../../../../../data/interim/2016/hyb_by_mon_calib_wide_shifted.rds")
+hyb_by_mon <- read_rds("../../../../../data/interim/2016/hyb_by_mon_calib_wide_shifted.rds")
 
-variance.var <- names(which(map_dbl(df[,16:255], var, na.rm = TRUE) != 0))
+variance.var <- names(which(map_dbl(hyb_by_mon[,16:255], var, na.rm = TRUE) != 0))
 mean.vars <- str_subset(variance.var, "mean")
 
 set.seed(1234)
-runInfoObj <- profRegr(covNames, outcome = 'Yield', yModel = 'Normal', xModel = "Mixed", discreteCovs = "Pedi", continuousCovs = mean.vars, data = df, nSweeps = 3000, nBurn = 50, nProgress = 100, nClusInit = 1000, seed = 2435)
+runInfoObj <- profRegr(covNames, outcome = 'Yield', yModel = 'Normal', xModel = "Mixed", 
+                       discreteCovs = "Pedi", continuousCovs = mean.vars, data = hyb_by_mon, nSweeps = 3000, 
+                       nBurn = 50, nProgress = 10, nClusInit = 1000, seed = 2435)
 calcDists <- calcDissimilarityMatrix(runInfoObj)
 clusObj <- calcOptimalClustering(calcDists)
 riskProfObj <- calcAvgRiskAndProfile(clusObj)
 write_rds(riskProfObj, "../riskProfObj.rds")
-
