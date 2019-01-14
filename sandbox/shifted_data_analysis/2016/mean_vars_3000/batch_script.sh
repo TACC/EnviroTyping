9ab1181b1527a0df31718fbcf0405c94f2bb94ee
Banks Osborne <banks@Bankss-MacBook-Pro.local>
2019-01-13 22:40:02 -0500

    Adapt

--- sandbox/shifted_data_analysis/2016/mean_vars_3000/batch_script.sh
+++ sandbox/shifted_data_analysis/2016/mean_vars_3000/batch_script.sh
@@ -2,12 +2,12 @@
 #
 #
 #SBATCH -J mean_3K_16
-#SBATCH -N 1
-#SBATCH -n 1
-#SBATCH --mail-user=azg5169@uncw.edu
+#SBATCH -N 2
+#SBATCH -n 4
+#SBATCH --mail-user=bno5761@uncw.edu
 #SBATCH --mail-type=all    # Send email at begin and end of job
 #SBATCH -p long
-#SBATCH -t 120:00:00
+#SBATCH -t 72:00:00
 #SBATCH -A Envriotype
 #SBATCH -o job_%j_%N.out
 #------------------------------------------------------
