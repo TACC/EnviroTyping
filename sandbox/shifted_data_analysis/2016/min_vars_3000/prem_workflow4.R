49e25d1c1fe79cf43d2bc96af603b13cc7bd1f3b
AustinGratton <grattonauz@gmail.com>
2019-01-29 13:35:08 -0500

    more seeds

--- sandbox/shifted_data_analysis/2016/min_vars_3000/prem_workflow4.R
+++ sandbox/shifted_data_analysis/2016/min_vars_3000/prem_workflow4.R
@@ -1,6 +1,8 @@
 library(PReMiuM)
 library(tidyverse)
 
+rm(list = ls(all = TRUE))
+
 setwd("/work/04902/azg5169/stampede2/EnviroTyping/sandbox/shifted_data_analysis/2016/min_vars_3000/output4")
 
 df <- read_rds("../../../../../data/interim/2016/hyb_by_mon_calib_wide_shifted.rds")
@@ -11,8 +13,7 @@ min.vars <- str_subset(variance.var, "min")
 set.seed(12345)
 
 
-runInfoObj <- profRegr(covNames, outcome = 'Yield', yModel = 'Normal', xModel = "Mixed", discreteCovs = "Pedi", continuousCovs = min.vars, data = df, nSweeps = 3000, nBurn = 50, nProgress = 1000, nClusInit = 1000, seed = 5800736320)
+runInfoObj <- profRegr(covNames, outcome = 'Yield', yModel = 'Normal', xModel = "Mixed", discreteCovs = "Pedi", continuousCovs = min.vars, data = df, nSweeps = 3000, nBurn = 1000, nProgress = 1000, nClusInit = 1000, seed = 9678240195)
 calcDists <- calcDissimilarityMatrix(runInfoObj)
 clusObj <- calcOptimalClustering(calcDists)
-riskProfObj <- calcAvgRiskAndProfile(clusObj)
 write_rds(clusObj, "../clusObj4.rds", compress = "xz")
