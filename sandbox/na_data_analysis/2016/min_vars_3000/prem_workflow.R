28d6043f9a6cb3c8587eec5f2ced3e56be2aad65
AustinGratton <grattonauz@gmail.com>
2019-04-24 11:27:54 -0400

    fix dimension issue with na data set

--- sandbox/na_data_analysis/2016/min_vars_3000/prem_workflow.R
+++ sandbox/na_data_analysis/2016/min_vars_3000/prem_workflow.R
@@ -3,13 +3,14 @@
 library(PReMiuM)
 library(tidyverse)
 
-setwd("/work/04902/azg5169/stampede2/EnviroTyping/sandbox/na_data_analysis/2016/min_vars_3000/output")
+#setwd("/work/04902/azg5169/stampede2/EnviroTyping/sandbox/na_data_analysis/2016/min_vars_3000/output")
+setwd("~/EnviroTyping/sandbox/na_data_analysis/2016/min_vars_3000/output")
 
 df <- read_rds("../../../../../data/interim/2016/hyb_by_mon_calib_w_wth_nas.rds")
 
 df = df[is.na(df$yield) == FALSE,]
 
-variance.var <- names(which(map_dbl(df[,16:255], var, na.rm = TRUE) != 0))
+variance.var <- names(which(map_dbl(df[,16:dim(df)[2]], var, na.rm = TRUE) != 0))
 min.vars <- str_subset(variance.var, "min")
 
 set.seed(12345)
