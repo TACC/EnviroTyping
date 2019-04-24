8c21ed9b765346a078b1f3a680c772e55186b6f3
AustinGratton <grattonauz@gmail.com>
2019-04-24 11:09:43 -0400

    2016 na data test for clustering at 500 initial

--- sandbox/na_data_analysis/2016/min_vars_3000/batch_script.sh
+++ sandbox/na_data_analysis/2016/min_vars_3000/batch_script.sh
@@ -5,7 +5,7 @@
 #SBATCH -N 1
 #SBATCH -n 1
 #SBATCH -p skx-normal
-#SBATCH -t 24:00:00
+#SBATCH -t 15:00:00
 #SBATCH -A Envriotype
 #SBATCH -o job_%j_%N.out
 #------------------------------------------------------
--- sandbox/na_data_analysis/2016/min_vars_3000/prem_workflow.R
+++ sandbox/na_data_analysis/2016/min_vars_3000/prem_workflow.R
@@ -1,4 +1,4 @@
-hyb_by_mon_calib_wide_w_wth_nas
+#hyb_by_mon_calib_w_wth_nas
 
 library(PReMiuM)
 library(tidyverse)
@@ -13,10 +13,9 @@ variance.var <- names(which(map_dbl(df[,16:255], var, na.rm = TRUE) != 0))
 min.vars <- str_subset(variance.var, "min")
 
 set.seed(12345)
-runInfoObj <- profRegr(covNames, outcome = 'yield', yModel = 'Normal', xModel = "Mixed", discreteCovs = "pedi", continuousCovs = min.vars, data = df, nSweeps = 3000, nBurn = 50, nProgress = 100, nClusInit = 1000, seed = 12345)
+runInfoObj <- profRegr(covNames, outcome = 'yield', yModel = 'Normal', xModel = "Mixed", discreteCovs = "pedi", continuousCovs = min.vars, data = df, nSweeps = 3000, nBurn = 1000, nProgress = 1000, nClusInit = 500)
 calcDists <- calcDissimilarityMatrix(runInfoObj)
 clusObj <- calcOptimalClustering(calcDists)
 riskProfObj <- calcAvgRiskAndProfile(clusObj)
-write_rds(riskProfObj, "../riskProfObj.rds")
 write_rds(clusObj, "../clusObj.rds", compress = "xz")
 
